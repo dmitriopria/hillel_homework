829acf51da2de67e54222642528828f12dd71d2e
dmitriopria <dimk824@gmail.com>
2023-03-06 07:58:54 -0500

    Class Car - created getters and setters for all fields. Class Employee - empty lines removed.

--- src/main/java/homework6/Car.java
+++ src/main/java/homework6/Car.java
@@ -20,12 +20,10 @@ public class Car {
     private void startElectricity() {
         System.out.println("Start electricity");
     }
-
     private void startCommand() {
         System.out.println("Start command");
 
     }
-
     private void startFuelSystem() {
         System.out.println("Start fuel system");
     }
--- src/main/java/homework6/Employee.java
+++ src/main/java/homework6/Employee.java
@@ -16,11 +16,42 @@ public class Employee {
         this.age = age;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public int getAge() {
         return age;
     }
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-    /* 2. Створити клас "Співробітник" з полями: ПІБ, посада, email, телефон, вік.
+/* 2. Створити клас "Співробітник" з полями: ПІБ, посада, email, телефон, вік.
     3. Конструктор класу повинен заповнювати ці поля під час створення об'єкта.
     Забезпечити інкапсуляцію внутрішніх властивостей класу. */
 
