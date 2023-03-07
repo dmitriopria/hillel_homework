0b0142f165bef455db30ce8d63814017a14c0f5d
dmitriopria <dimk824@gmail.com>
2023-03-07 16:53:30 -0500

    HW 6 final commit, formatting of class Car.

--- src/main/java/homework6/Car.java
+++ src/main/java/homework6/Car.java
@@ -20,10 +20,11 @@ public class Car {
     private void startElectricity() {
         System.out.println("Start electricity");
     }
+
     private void startCommand() {
         System.out.println("Start command");
-
     }
+
     private void startFuelSystem() {
         System.out.println("Start fuel system");
     }
