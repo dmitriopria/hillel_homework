014c3d45723a6b325ab866dea103fbc19d6483d9
dmitriopria <dimk824@gmail.com>
2023-03-07 16:37:16 -0500

    Second commit of branch homework7, with package 'abstractTest' where stored my test of abstract class decision.

---
+++ src/main/java/homework7/abstractTest/Dog.java
@@ -0,0 +1,40 @@
+package homework7.abstractTest;
+
+public class Dog extends Animal {
+
+    private int maxRunDistance = 500;
+    private int maxSwimDistance = 10;
+    private static int dogCount = 0;
+
+    public Dog(String name) {
+        super(name);
+        dogCount++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance <= 0) {
+            System.out.println("Enter valid number");
+        } else if (distance <= maxRunDistance) {
+            System.out.println(name + " ran " + distance + " m.");
+        } else {
+            System.out.println(name + " couldn't run " + distance + " m.");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance <= 0) {
+            System.out.println("Enter valid number");
+        } else if (distance <= maxSwimDistance) {
+            System.out.println(name + " swam " + distance + " m.");
+        } else {
+            System.out.println(name + " couldn't swim " + distance + " m.");
+        }
+    }
+
+    public static int getDogCount() {
+        return dogCount;
+    }
+
+}
