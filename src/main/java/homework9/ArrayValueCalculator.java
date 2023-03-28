0e79bb0dc79d103fe7e14eef6bb2d078f0c842de
dmitriopria <dimk824@gmail.com>
2023-03-27 21:35:47 -0400

    HW 9 final corrections - isMatrixSizeCorrect() method added

--- src/main/java/homework9/ArrayValueCalculator.java
+++ src/main/java/homework9/ArrayValueCalculator.java
@@ -3,11 +3,7 @@ package homework9;
 public class ArrayValueCalculator {
 
     public int doCalc(String[][] inputArray) {
-        if (inputArray.length != 4 ||
-                inputArray[0].length != 4 ||
-                inputArray[1].length != 4 ||
-                inputArray[2].length != 4 ||
-                inputArray[3].length != 4) {
+        if (isMatrixSizeCorrect(inputArray)) {
             throw new ArraySizeException("Input array size should be 4X4 - 4 arrays must contain 4 elements each");
         }
         int arraySum = 0;
@@ -16,9 +12,9 @@ public class ArrayValueCalculator {
                 try {
                     arraySum += Integer.parseInt(inputArray[i][j]);
                 } catch (NumberFormatException e) {
-                    int firsIndexCorr = i + 1;
-                    int secondIndexCorr = j + 1;
-                    throw new ArrayDataException("Invalid data in cell (" + firsIndexCorr + "," + secondIndexCorr + "): " +
+                    int firsIndex = i + 1;
+                    int secondIndex = j + 1;
+                    throw new ArrayDataException("Invalid data in cell (" + firsIndex + "," + secondIndex + "): " +
                             inputArray[i][j] + " - all array cells must be only numbers");
                 }
             }
@@ -26,4 +22,11 @@ public class ArrayValueCalculator {
         return arraySum;
     }
 
+    private boolean isMatrixSizeCorrect(String[][] inputArray) {
+        return inputArray.length != 4 ||
+                inputArray[0].length != 4 ||
+                inputArray[1].length != 4 ||
+                inputArray[2].length != 4 ||
+                inputArray[3].length != 4;
+    }
 }
