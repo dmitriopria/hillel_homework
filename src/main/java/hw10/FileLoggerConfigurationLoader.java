9b153fdd8907969810c483bd10fa5678d8bdaffe
dmitriopria <dimk824@gmail.com>
2023-04-11 10:30:02 -0400

    Homework 10 - unnecessary comment was deleted

--- src/main/java/hw10/FileLoggerConfigurationLoader.java
+++ src/main/java/hw10/FileLoggerConfigurationLoader.java
@@ -25,26 +25,3 @@ public class FileLoggerConfigurationLoader {
         }
     }
 }
-//        Objects.requireNonNull(filePath);
-//        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-//            String line;
-//            File file = null;
-//            long maxFileSize = 0;
-//            LoggingLevel loggingLevel = null;
-//            String messageFormat = "";
-//            while ((line = reader.readLine()) != null) {
-//                String[] parts = line.split("=");
-//                String key = parts[0].trim();
-//                String value = parts[1].trim();
-//                switch (key) {
-//                    case "file" -> file = new File(value);
-//                    case "level" -> loggingLevel = LoggingLevel.valueOf(value);
-//                    case "size" -> maxFileSize = Long.parseLong(value);
-//                    case "format" -> messageFormat = value;
-//                }
-//            }
-//            return new FileLoggerConfiguration(file, loggingLevel, maxFileSize, messageFormat);
-//        } catch (IOException e) {
-//            throw new LoadConfigFileException("Error of loading configuration file: " + e.getMessage());
-//        }
-//    }
