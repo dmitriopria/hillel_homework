899963a75a5a5341e5f8b87a39c98ac878672e5e
dmitriopria <dimk824@gmail.com>
2023-05-28 09:20:37 -0400

    Homework 20 added more threads for testing

--- src/main/java/hw20/Main.java
+++ src/main/java/hw20/Main.java
@@ -1,17 +1,26 @@
 package hw20;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        PetrolStation station = new PetrolStation(100);
-        FuelDispenser dispenser1 = new FuelDispenser(station, 30);
-        FuelDispenser dispenser2 = new FuelDispenser(station, 10);
-        FuelDispenser dispenser3 = new FuelDispenser(station, 20);
-        try (ExecutorService executor = Executors.newFixedThreadPool(3)) {
-            executor.invokeAll(List.of(dispenser1, dispenser2, dispenser3));
+        PetrolStation station = new PetrolStation(1000);
+        List<FuelDispenser> dispensers = new ArrayList<>();
+        dispensers.add(new FuelDispenser(station, 30));
+        dispensers.add(new FuelDispenser(station, 10));
+        dispensers.add(new FuelDispenser(station, 20));
+        dispensers.add(new FuelDispenser(station, 60));
+        dispensers.add(new FuelDispenser(station, 70));
+        dispensers.add(new FuelDispenser(station, 20));
+        dispensers.add(new FuelDispenser(station, 10));
+        dispensers.add(new FuelDispenser(station, 15));
+        dispensers.add(new FuelDispenser(station, 25));
+        dispensers.add(new FuelDispenser(station, 20));
+        try (ExecutorService executor = Executors.newFixedThreadPool(dispensers.size())) {
+            executor.invokeAll(dispensers);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
