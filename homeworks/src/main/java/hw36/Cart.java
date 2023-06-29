c9771c728bae29770d7589c1d76fa3c1618f750f
Dmitri Opria <dimk824@gmail.com>
2023-06-29 08:30:42 -0400

    HW 36 commit after correction

--- homeworks/src/main/java/hw36/Cart.java
+++ homeworks/src/main/java/hw36/Cart.java
@@ -1,13 +1,14 @@
 package hw36;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Component
+@Scope("prototype")
 public class Cart {
     private final List<Product> products;
 
@@ -23,10 +24,10 @@ public class Cart {
         if (id < 1) {
             throw new RuntimeException("Product ID can't be negative or zero!");
         }
-        Optional<Product> productToRemove = products.stream()
+        products.stream()
                 .filter(product -> product.getId() == id)
-                .findFirst();
-        productToRemove.ifPresent(products::remove);
+                .findAny()
+                .ifPresent(products::remove);
     }
 
     public List<Product> getProducts() {
--- homeworks/src/main/java/hw36/Product.java
+++ homeworks/src/main/java/hw36/Product.java
@@ -7,13 +7,13 @@ public class Product {
     private String name;
     private double price;
 
+    public Product() {
+    }
+
     public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
-    }
-
-    public Product() {
     }
 
     public int getId() {
